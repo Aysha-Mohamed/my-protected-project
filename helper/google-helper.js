6ad16313abf1fa2d1d340dd1c6c8072cf4411066
Aysha <42186490+Aysha-Mohamed@users.noreply.github.com>
2025-08-05 12:58:49 +0300

    Create google-helper.js

---
+++ helper/google-helper.js
@@ -0,0 +1,66 @@
+const { google } = require('googleapis');
+const { JWT } = require('google-auth-library');
+
+class GoogleHelper {
+  constructor(keyFile = 'key.json') {
+    this.auth = new JWT({
+      keyFile,
+      scopes: [
+        'https://www.googleapis.com/auth/drive.readonly',
+        'https://www.googleapis.com/auth/documents.readonly',
+      ],
+    });
+
+    this.drive = google.drive({ version: 'v3', auth: this.auth });
+    this.docs = google.docs({ version: 'v1', auth: this.auth });
+  }
+
+  async listDocFilesInFolder(folderId) {
+    const q = [
+      `'${folderId}' in parents`,
+      `mimeType = 'application/vnd.google-apps.document'`,
+      `trashed = false`,
+    ].join(' and ');
+
+    let files = [];
+    let pageToken;
+
+    do {
+      const { data } = await this.drive.files.list({
+        q,
+        pageSize: 1000,
+        pageToken,
+        fields: 'nextPageToken, files(id, name, createdTime, modifiedTime, version)',
+        supportsAllDrives: true,
+        includeItemsFromAllDrives: true,
+        corpora: 'allDrives',
+      });
+
+      files = files.concat(data.files || []);
+      pageToken = data.nextPageToken;
+    } while (pageToken);
+
+    return files;
+  }
+
+  async getDocument(documentId) {
+    const { data } = await this.docs.documents.get({ documentId });
+    return data;
+  }
+
+  async getFileVersion(fileId) {
+    const { data } = await this.drive.files.get({
+      fileId,
+      fields: 'version',
+      supportsAllDrives: true,
+    });
+    return data.version;
+  }
+
+  async versionUnchanged(fileId, beforeVersion) {
+    const currentVersion = await this.getFileVersion(fileId);
+    return currentVersion === beforeVersion;
+  }
+}
+
+module.exports = GoogleHelper;
